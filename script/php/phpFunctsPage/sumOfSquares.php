a9d8f9214ed23d82c2cbe1df595e3592665faf40
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-04-03 21:44:24 -0500

    fix for sumOfSquares

--- script/php/phpFunctsPage/sumOfSquares.php
+++ script/php/phpFunctsPage/sumOfSquares.php
@@ -8,5 +8,6 @@
             }
                 return "The sum of squares between ".$_GET['smaller_int']." and ".$_GET['larger_int']." is ".$temp;
         }else if($a==$b){
-            return "The sum of squares of these two numbers is 0";}else{return "Please make sure the Larger Integer box is a *larger integer* than the smaller integer";}}sumOfSquares($_GET['larger_int'], $_GET['smaller_int']);
+            return "The sum of squares of these two numbers is 0";}else{return "Please make sure the Larger Integer box is a *larger integer* than the smaller integer";}}
+    echo sumOfSquares($_GET['larger_int'], $_GET['smaller_int']);
 ?>
