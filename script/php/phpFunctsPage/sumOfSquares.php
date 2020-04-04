e0a478e30c79293e1b64602786961439148c249d
ac25ville <55512495+ac25ville@users.noreply.github.com>
2020-04-03 21:43:33 -0500

    fix for sumOfSquares

--- script/php/phpFunctsPage/sumOfSquares.php
+++ script/php/phpFunctsPage/sumOfSquares.php
@@ -4,10 +4,9 @@
             $temp=0;
             
             for($i = $b; $i<=$a; $i++){
-                $temp+= $i*$i;
+                $temp+= $i*$i;//make sure to use the dollar signs EVERYTIME you use a variable smh :/
             }
-                return $temp;
+                return "The sum of squares between ".$_GET['smaller_int']." and ".$_GET['larger_int']." is ".$temp;
         }else if($a==$b){
-            return "The sum of squares of these two numbers is 0";}else{return "Please make sure the Larger Integer box is a *larger integer* than the smaller integer";}
-    }echo "The sum of squares between ".$_GET['smaller_int']." and ".$_GET['larger_int']." is ".sumOfSquares($_GET['larger_int'], $_GET['smaller_int']);
+            return "The sum of squares of these two numbers is 0";}else{return "Please make sure the Larger Integer box is a *larger integer* than the smaller integer";}}sumOfSquares($_GET['larger_int'], $_GET['smaller_int']);
 ?>
